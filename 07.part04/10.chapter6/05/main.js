519c1ab9063dec4b8935cd96aa0a2dff300e9aed
Lincoln Brito <lincoln.sbrito@gmail.com>
2014-09-25 00:18:22 -0300

    link and compile functions

---
+++ 07.part04/10.chapter6/05/main.js
@@ -0,0 +1,25 @@
+function SomeController($scope){
+	$scope.title = 'Click me to expand';
+	$scope.text = 'Hi folks, I am the content that was hidden but is no shown.';
+}
+
+angular.module("expanderModule", [])
+	.directive('expander', function(){
+		return {
+			restrict: 'EA',
+			replace: true,
+			transclude: true,
+			scope: { title: '=expanderTitle' },
+			template: '<div>'+
+				'<div class="title" ng-click="toggle()">{{title}}</div>'+
+				'<div class="body" ng-show="showMe" ng-transclude></div>'+
+			'</div>',
+			link: function(scope, element, attrs){
+				scope.showMe = false;
+
+				scope.toggle = function toggle(){
+					scope.showMe = !scope.showMe;
+				}	
+			}
+		};
+	});
