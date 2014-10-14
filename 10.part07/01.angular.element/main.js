ab70670c96a896e40acc11cab684c48459e4823d
Lincoln Brito <lincoln.sbrito@gmail.com>
2014-10-14 17:17:55 -0300

    part07

---
+++ 10.part07/01.angular.element/main.js
@@ -0,0 +1,28 @@
+var app = angular.module("app",[]);
+
+app.directive("dumbPassword", function(){
+	var validElement = angular.element("<div>{{model.input}}</div>");
+	
+	this.link = function (scope) {
+		scope.$watch("model.input", function(value){
+			if(value === "password"){
+				validElement.toggleClass("alert alert-info");
+			}
+		})
+	}
+
+	return {
+		restrict: "E",
+		replace: true,
+		template: "<div>"+
+			"<input type='text' ng-model='model.input' />"+
+			
+		"</div>",
+		compile: function(tElem){
+			tElem.append(validElement);
+
+			return link;
+		},		
+	}
+});
+
